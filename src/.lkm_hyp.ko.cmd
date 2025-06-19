47bed95e758600e6ed16fc61783ba22f99d55424
ChrinovicMu <chrinovicmu@gmail.com>
2025-06-19 23:06:20 +0200

    remove garbage build files

--- src/.lkm_hyp.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/chrinovic/Tech/Hypervisors/Hypervisor-linux/src/lkm_hyp.ko := ld -r -m elf_x86_64 -z noexecstack --no-warn-rwx-segments --build-id=sha1  -T arch/x86/module.lds -o /home/chrinovic/Tech/Hypervisors/Hypervisor-linux/src/lkm_hyp.ko /home/chrinovic/Tech/Hypervisors/Hypervisor-linux/src/lkm_hyp.o /home/chrinovic/Tech/Hypervisors/Hypervisor-linux/src/lkm_hyp.mod.o;  true
